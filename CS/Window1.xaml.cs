27a4347ce42ccd19ac03d87379181116389cb7db
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace ManhattanBar3DChart {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+}
